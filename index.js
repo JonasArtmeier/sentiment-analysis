4f0b90cebf11dfa8d65715f67be69a99051fd41b
Jonas Artmeier <jonas-artmeier@web.de>
2020-05-08 18:23:06 +0200

    Add code to get the sentiment index

--- index.js
+++ index.js
@@ -1,49 +1,25 @@
 const fetch = require('node-fetch');
-
-// fetch('https://apidemo.theysay.io/api/v1/sentiment', {
-//   headers: {
-//     accept: 'application/json',
-//     'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
-//     'content-type': 'application/json',
-//     'sec-fetch-dest': 'empty',
-//     'sec-fetch-mode': 'cors',
-//     'sec-fetch-site': 'same-origin',
-//     'x-requested-with': 'XMLHttpRequest',
-//     // cookie: '_ga=GA1.2.251480195.1588850332; _gid=GA1.2.1031969901.1588850332',
-//     'User-Agent':
-//       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
-//   },
-//   referrer: 'https://apidemo.theysay.io/',
-//   referrerPolicy: 'no-referrer-when-downgrade',
-//   body: '{"text":"Hallo wie gehts?\\nhfgh","level":"sentence"}',
-//   method: 'POST',
-//   // mode: 'cors',
-// })
-//   .then((res) => res.text())
-//   .then((body) => console.log(body));
-
 fetch('https://apidemo.theysay.io/api/v1/sentiment', {
   headers: {
     accept: 'application/json',
-    Origin: 'https://apidemo.theysay.io',
     Connection: 'keep-alive',
-    UserAgent:
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36',
-
-    acceptlanguage: 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
-    contenttype: 'application/json',
-    secfetchdest: 'empty',
-    secfetchmode: 'cors',
-    secfetchsite: 'same-origin',
-    xrequestedwith: 'XMLHttpRequest',
-    Referer: 'https://apidemo.theysay.io/',
+    Accept: 'application/json',
+    // 'accept-language': 'en-US,en;q=0.9',
+    'content-type': 'application/json',
+    Origin: 'https://apidemo.theysay.io',
+    // 'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    // 'sec-fetch-site': 'same-origin',
+    'x-requested-with': 'XMLHttpRequest',
+    cookie: '_ga=GA1.2.491599625.1588849875; _gid=GA1.2.35036145.1588849875',
+    'User-Agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+    referer: 'https://apidemo.theysay.io/',
   },
-  referrer: 'https://apidemo.theysay.io/',
-  referrerPolicy: 'no-referrer-when-downgrade',
-  body: '{"text":"Hallo wie gehts?"}',
+  // referrerPolicy: 'no-referrer-when-downgrade',
+  body: '{"text":"sad ass"}',
   method: 'POST',
   mode: 'cors',
-  credentials: 'include',
 })
   .then((res) => res.text())
   .then((body) => console.log(body));
